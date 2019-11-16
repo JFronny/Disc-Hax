b45bca1b172068f0075ecf6c44edc46f0f0d3199
CreepyCrafter24 <33260128+CreepyCrafter24@users.noreply.github.com>
2019-11-16 18:51:51 +0100

    stuffs

--- Bot/Properties/Resources.Designer.cs
+++ Bot/Properties/Resources.Designer.cs
@@ -59,14 +59,5 @@ namespace Bot.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to .
-        /// </summary>
-        internal static string HIDClasses {
-            get {
-                return ResourceManager.GetString("HIDClasses", resourceCulture);
-            }
-        }
     }
 }
